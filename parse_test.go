b0e2c143c3c52942ce34a9feb2db1171a928cbd3
Chris Wilding <hello@chriswilding.co.uk>
2022-03-08 17:07:16 +0000

    Parse HTML and extract a link

---
+++ parse_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var exampleHTML = `
+<!doctype html>
+<html>
+<head>
+    <title>Example Domain</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+
+<body>
+<div>
+    <h1>Example Domain</h1>
+    <p>This domain is for use in illustrative examples in documents. You may use this
+    domain in literature without prior coordination or asking for permission.</p>
+    <p><a href="https://www.iana.org/domains/example">More information...</a></p>
+</div>
+</body>
+</html>
+`
+
+func TestParse(t *testing.T) {
+	expected := []string{
+		"https://www.iana.org/domains/example",
+	}
+
+	reader := strings.NewReader(exampleHTML)
+	actual, err := parse(reader)
+
+	assert.Nil(t, err)
+	assert.ElementsMatch(t, actual, expected)
+}
