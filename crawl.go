2ac9120e74ff7a4e27adb43b7b6ab87f19007278
Chris Wilding <hello@chriswilding.co.uk>
2022-03-08 17:32:51 +0000

    Outline a plan

---
+++ crawl.go
@@ -0,0 +1,12 @@
+package main
+
+func crawl(url string) {
+	// GET the page
+	// Parse the HTML and extract the links
+	// Print the URL
+	// Print each of the links
+	// Filter the links so that the slice only contains links on the same domain
+	// If not already visited
+	//   Crawl the link
+	//   Mark as visited
+}
